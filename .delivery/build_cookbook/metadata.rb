f7802dcf0e3612e5802a56dc08d250dbb3c96e47
Jonathan Idica <jonathan.idica@gmail.com>
2017-11-08 20:33:58 -1000

    Add generated delivery build cookbook

---
+++ .delivery/build_cookbook/metadata.rb
@@ -0,0 +1,8 @@
+name 'build_cookbook'
+maintainer 'Jonathan Idica'
+maintainer_email 'jonathan.idica@gmail.com'
+license 'apachev2'
+version '0.1.0'
+chef_version '>= 12.1' if respond_to?(:chef_version)
+
+depends 'delivery-truck'
